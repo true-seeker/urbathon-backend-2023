9795c08f02d5833555eecee09fbf3dc7da177dd8
Kirill Murzin <murz.kirill@yandex.ru>
2023-11-20 17:52:59 +0500

    compose migration and codegen

---
+++ deployments/startup.sh
@@ -0,0 +1,4 @@
+jet -dsn="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable" -schema=public -path=.gen
+cd cmd/urbathon-backend-2023
+go build
+./urbathon-backend-2023 -srvAddr=webapi -srvPort=8080
