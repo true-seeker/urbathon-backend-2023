5ab66264e0f49c665c11e84a350319e90f75162e
Kirill Murzin <murz.kirill@yandex.ru>
2023-11-23 18:23:53 +0500

    next page url nil added

--- internal/app/model/response/Paged.go
+++ internal/app/model/response/Paged.go
@@ -3,15 +3,25 @@ package response
 import "fmt"
 
 type Paged struct {
-	Page        int64  `json:"page" example:"1"`
-	PageSize    int64  `json:"page_size" example:"10"`
-	Total       int    `json:"total" example:"100"`
-	NextPageUrl string `json:"next_page_url" example:"/api/news?page=1&page_size=10"`
+	Page        int64   `json:"page" example:"1"`
+	PageSize    int64   `json:"page_size" example:"10"`
+	Total       int     `json:"total" example:"100"`
+	NextPageUrl *string `json:"next_page_url" example:"/api/news?page=1&page_size=10"`
 }
 
 func NewPaged(page int64, pageSize int64, total int, resource string) *Paged {
+	var nextPageUrlP *string
+	fmt.Println(resource, page+1, pageSize, nextPageUrlP)
+	if pageSize*page < int64(total) {
+		temp := fmt.Sprintf("/api/%s?page=%d&page_size=%d",
+			resource,
+			page+1,
+			pageSize)
+		nextPageUrlP = &temp
+	}
+
 	return &Paged{Page: page,
 		PageSize:    pageSize,
 		Total:       total,
-		NextPageUrl: fmt.Sprintf("/api/%s?page=%d&page_size=%d", resource, page+1, pageSize)}
+		NextPageUrl: nextPageUrlP}
 }
